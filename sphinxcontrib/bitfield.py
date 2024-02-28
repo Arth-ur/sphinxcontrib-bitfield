33e72990a3534762e5a7b22708a57f0929c7bb28
tyler miller <tmiller@ara-inc.com>
2024-02-28 10:23:27 -0500

    Support adding captions to bitfield directives
    This will render it as a figure in the docs like graphviz

--- sphinxcontrib/bitfield.py
+++ sphinxcontrib/bitfield.py
@@ -1,16 +1,30 @@
 from docutils import nodes
-from docutils.parsers.rst import Directive
+from docutils.parsers.rst import Directive, directives
 from docutils.parsers.rst.directives import flag
 from bit_field import render, jsonml_stringify
 from json import loads
 from shlex import split
 from base64 import b64encode
 from sphinx import addnodes
+from sphinx.util.nodes import set_source_info
 
 
 def legend(s):
     x = split(s)
     return {k: v for k, v in zip(x[0::2], x[1::2])}
+
+
+def figure_wrapper(directive, children, caption) -> nodes.figure:
+    # based on the figure_wrapper method in sphinx.ext.graphviz
+    # https://github.com/sphinx-doc/sphinx/blob/master/sphinx/ext/graphviz.py
+    figure_node = nodes.figure('', *children)
+
+    inodes, messages = directive.state.inline_text(caption, directive.lineno)
+    caption_node = nodes.caption(caption, '', *inodes)
+    caption_node.extend(messages)
+    set_source_info(directive, caption_node)
+    figure_node += caption_node
+    return figure_node
 
 
 class BitfieldDirective(Directive):
@@ -30,9 +44,14 @@ class BitfieldDirective(Directive):
         'uneven': flag,
         'trim': float,
         'legend': legend,
+        'caption': directives.unchanged,
     }
 
     def run(self):
+        # remove 'caption' since its only used on the sphinx side and bit_field
+        # doesnt recognize it
+        caption = self.options.pop('caption', None)
+
         svg = jsonml_stringify(
             render(
                 loads(' '.join(self.content)),
@@ -45,7 +64,15 @@ class BitfieldDirective(Directive):
         onlynode = addnodes.only(expr='latex')
         onlynode += nodes.image('', uri=uri)
 
-        return [nodes.raw('', svg, format='html'), onlynode]
+        content = [nodes.raw('', svg, format='html'), onlynode]
+
+        if caption is None:
+            return content
+
+        # wrap as a figure
+        fig_node = figure_wrapper(self, content, caption)
+        self.add_name(fig_node)
+        return [fig_node]
 
 
 def setup(app):
